79bc1f2df47cd7c5c1bea15a5e2bf90286024e60
Marco Chen <zozowind@gmail.com>
2020-08-18 10:20:49 +0800

    增加返回image功能

--- poster/index.go
+++ poster/index.go
@@ -2,6 +2,7 @@ package poster
 
 import (
 	"fmt"
+	"image"
 
 	"github.com/fogleman/gg"
 	"github.com/zozowind/image-mix/elements"
@@ -15,29 +16,50 @@ type Poster struct {
 	Background string  `json:"background"` //背景颜色
 	Alpha      float64 `json:"alpha"`
 	Elements   []elements.Element
+	Context    *gg.Context
 }
 
-//DrawToFile 保存到文件
-func (p *Poster) DrawToFile(path string) (err error) {
+//Draw 绘制海报
+func (p *Poster) Draw() (err error) {
 	//构建画布
-	g := gg.NewContext(p.W, p.H)
+	p.Context = gg.NewContext(p.W, p.H)
 	//设置背景色
 
-	g.DrawRectangle(0, 0, float64(p.W), float64(p.H))
+	p.Context.DrawRectangle(0, 0, float64(p.W), float64(p.H))
 	if p.Background != "" {
-		err = util.SetColor(g, p.Background, p.Alpha)
+		err = util.SetColor(p.Context, p.Background, p.Alpha)
 		if nil != err {
-			return err
+			return
 		}
 	}
-	g.Fill()
+
+	p.Context.Fill()
 
 	for _, ele := range p.Elements {
-		err = ele.Draw(g)
+		err = ele.Draw(p.Context)
 		if nil != err {
 			fmt.Println(err.Error())
 		}
 	}
-	err = g.SavePNG(path)
+	return
+}
+
+//DrawToImage 返回Image
+func (p *Poster) DrawToImage() (img image.Image, err error) {
+	err = p.Draw()
+	if nil != err {
+		return
+	}
+	img = p.Context.Image()
+	return
+}
+
+//DrawToFile 保存到文件
+func (p *Poster) DrawToFile(path string) (err error) {
+	err = p.Draw()
+	if nil != err {
+		return
+	}
+	err = p.Context.SavePNG(path)
 	return
 }
